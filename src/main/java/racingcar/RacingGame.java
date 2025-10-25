0727b5803129d4dc9daf5c19b2c3b72d8437b8ef
chan <djc0807285@gmail.com>
2025-10-26 03:42:09 +0900

    feat: 자동차 이름 유효성 검사 기능 구현

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class RacingGame {
+
+    public void run() {
+
+        //1. 자동차 입력
+        //2. 이름 유효성 검사
+        //3. 자동차 생성
+        //4. 시도 횟수 입력
+        //5. 시도 횟수 유효성 검사
+        //6. 경주 실행
+        //7. 우승자 출력
+    }
+}
